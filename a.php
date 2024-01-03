57575628b02df852ef9834068c7158b88de5477c
kwy2559 <152370972+kwy2559@users.noreply.github.com>
2024-01-03 13:56:49 +0700

    Example1 in php
    example1 in php

---
+++ a.php
@@ -0,0 +1,9 @@
+<?php
+    $a=4;
+    $b=10;
+
+    echo "Result 1 =" . ($a + $b) . "<br>";
+    echo "Result 1 =" . ($b - $a) . "<br>";
+    echo "Result 1 =" . ($a * $b) . "<br>";
+    echo "Result 1 =" . ($b / $a) . "<br>";
+?>
