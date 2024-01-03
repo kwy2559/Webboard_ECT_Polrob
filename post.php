d0cde8dfe35ceb0504c7754ee33a4f61031236ef
kwy2559 <152370972+kwy2559@users.noreply.github.com>
2024-01-03 16:29:52 +0700

    Polrob.php
    polrob.php

---
+++ post.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webboard KaKKaK</title>
+    <h1 style="text-align: center;">Webboard KaKKaK</h1>
+    <hr>
+</head>
+<body>
+    <div style="text-align: center;">ต้องการดูกระทู้หมายเลข <?php echo $_GET['id']; ?></div>
+    <div>
+    <br>
+        <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr style="background-color: #6CD2FE"><td>แสดงความคิดเห็น</td></tr>
+        <tr><td align="center"><textarea cols="50" rows="10"></textarea></td><td>
+        <tr><td align="center" colspan="2"><input type="submit"value="ส่งข้อความ"></td></tr>
+        </table>
+    </div><br>
+    <div align="center"><a href="register.html">กลับไปยังหน้าหลัก</a></div>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <form>
+    <table style="border: 2px solid black; width: 40%;" align="center">   
+    <h1 style="text-align: center;">Webboard Polrob</h1>
+    <hr>
+    <div style="text-align: center;">
+    เข้าสู่ระบบด้วย<br>
+    Login =<?php echo $_POST['login']?><br>
+    Password =<?php echo $_POST['pwd']?><br>
+</div>
+</table>
+    </form>    
+</body>
+</html>
