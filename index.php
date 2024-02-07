ea67f8b91cb02bf7a2bbba4cfb86535f16a172cd
kwy2559 <152370972+kwy2559@users.noreply.github.com>
2024-02-07 16:54:41 +0700

    add bootstrap follow work7
    add bootstrap follow work7

--- index.php
+++ index.php
@@ -6,12 +6,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=>, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard Polrob</title>
     
 </head>
 <body>
+    <div class="container">
     <h1 style="text-align: center;">Webboard Polrob</h1>
     <hr>
+    <nav class="navbar navbar-expand-lg " style="background-color: #d3d3d3;">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+    
+    
+      <div class="navbar-nav">
+        <?php if(!isset($_SESSION['id'])){?>
+        <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{?>
+            <li class="nav-item dropdown">
+          <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username']; ?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
+        </li>
+            <?php } ?>
+      </div> 
+  </div>
+</nav>
     <from>
         หมวดหมู่ :
         <select>
@@ -45,7 +72,7 @@
             }
         ?>    
         </ul>
-
+        </div>
 
 </body>
 </html>
